2b906ad27ba15114bb7cae6a82625697c4fff643
Mateusz Margos <mateuszmargos@gmail.com>
2018-02-28 22:28:51 +0100

    Finished navigation for mobile, added main photo.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // script.js
 
 $(".nav_button").click(function() {
+  toggleNav();
+});
+
+$("main").click(function() {
+  if($("main").hasClass("nav_opened"))
+  toggleNav();
+});
+
+function toggleNav() {
   $("nav").toggleClass("nav_opened");
   $(".nav_button").toggleClass("nav_opened");
+  $(".nav_button_arrow").toggleClass("nav_opened");
   $("main").toggleClass("nav_opened");
-});
+}
