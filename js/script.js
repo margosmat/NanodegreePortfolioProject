7bffe3dd561815d784f38c67556a0b0b733990a8
Mateusz Margos <mateuszmargos@gmail.com>
2018-03-03 15:11:34 +0100

    Style: code cleanup, preparation for project submit.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,9 @@ $("section").click(function() {
   toggleNav();
 });
 
+/**
+* @description Adds "nav_opened" class to specified elements
+*/
 function toggleNav() {
   $("nav").toggleClass("nav_opened");
   $(".nav_button").toggleClass("nav_opened");
